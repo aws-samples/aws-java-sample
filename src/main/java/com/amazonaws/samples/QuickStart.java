17843edce54bb7ffa32042953d9e1068dd6af41d
Andrew Fitz Gibbon <gibbon@amazon.com>
2013-08-27 16:57:15 -0700

    Init commit of pom and source

---
+++ src/main/java/com/amazonaws/samples/QuickStart.java
@@ -0,0 +1,23 @@
+package com.amazonaws.samples;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+ 
+public class QuickStart {
+
+    public static void main(String[] args) {
+        // Load the credentials file
+        AWSCredentialsProvider credentialsProvider
+            = new ClasspathPropertiesFileCredentialsProvider();
+
+        // Instantiate an S3 client
+        AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
+
+        // Create a bucket
+        String bucketName = "aws-java-sdk-sample-" + credentialsProvider.getCredentials().getAWSAccessKeyId();
+        s3.createBucket(bucketName);
+    }
+
+}
